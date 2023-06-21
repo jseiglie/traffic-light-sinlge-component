3f71599e68f57ae74e76f16a0bd882809caf718b
jseiglie <javierseiglie@gmail.com>
2023-06-21 15:50:22 +0000

    chore: css updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import './App.css';
-import React, { useState } from 'react';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
-  const [glow, setGlow] = useState("")
+  const [glow, setGlow] = useState("");
 
   return (
-    <div className="App" >
-      <div className={`light red ${glow === "red" ? "glow" : ""}`} 
-      onClick={() => {
-        setGlow("red")
-      }} 
-      ></div>
-      <div className={`light yellow ${glow === "yellow" ? "glow" : ""}`} 
-      onClick={() => {
-        setGlow("yellow")
-      }}
-       ></div>
-      <div className={`light green ${glow === "green" ? "glow" : ""}`}
-        onClick={() => 
-          setGlow("green"
-          )}
-      ></div>
+    <div className="App">
+      <div className="wrapper">
+        <div
+          className={`light red ${glow === "red" ? "glow" : ""}`}
+          onClick={() => {
+            setGlow("red");
+          }}
+        ></div>
+        <div
+          className={`light yellow ${glow === "yellow" ? "glow" : ""}`}
+          onClick={() => {
+            setGlow("yellow");
+          }}
+        ></div>
+        <div
+          className={`light green ${glow === "green" ? "glow" : ""}`}
+          onClick={() => setGlow("green")}
+        ></div>
+      </div>
     </div>
   );
 }
