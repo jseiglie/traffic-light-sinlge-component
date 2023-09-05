d1c03cbcf1b7c794b7265363b846836ed88ea50f
Javier Seiglie <96433186+jseiglie@users.noreply.github.com>
2023-09-05 11:26:01 +0000

    added pruple light

--- src/App.js
+++ src/App.js
@@ -3,28 +3,36 @@ import React, { useState } from "react";
 
 function App() {
   const [glow, setGlow] = useState("");
-
+  const [show, setShow] = useState(false)
   return (
-    <div className="App">
-      <div className="wrapper">
-        <div
-          className={`light red ${glow === "red" ? "glow" : ""}`}
-          onClick={() => {
-            setGlow("red");
-          }}
-        ></div>
-        <div
-          className={`light yellow ${glow === "yellow" ? "glow" : ""}`}
-          onClick={() => {
-            setGlow("yellow");
-          }}
-        ></div>
-        <div
-          className={`light green ${glow === "green" ? "glow" : ""}`}
-          onClick={() => setGlow("green")}
-        ></div>
+    <>
+      <div className="App">
+      <button className="btn btn-primary" onClick={()=>show? setShow(false) : setShow(true)}>Purple</button>
+
+        <div className="wrapper">
+          <div
+            className={`light red ${glow === "red" ? "glow" : ""}`}
+            onClick={() => {
+              setGlow("red");
+            }}
+          ></div>
+          <div
+            className={`light yellow ${glow === "yellow" ? "glow" : ""}`}
+            onClick={() => {
+              setGlow("yellow");
+            }}
+          ></div>
+          <div
+            className={`light green ${glow === "green" ? "glow" : ""}`}
+            onClick={() => setGlow("green")}
+          ></div>
+          <div className={`light purple ${show ? "show" : "hidden"} ${glow === "purple" ? "glow" : ""} `}
+          onClick={()=>setGlow("purple")}
+          >
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
